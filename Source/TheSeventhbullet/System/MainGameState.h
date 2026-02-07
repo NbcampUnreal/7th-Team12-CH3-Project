3324ec74f66d6a2b0ad27356d060c7b570c27e43
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-02-07 20:53:16 +0900

    System Raw Class Init

---
+++ Source/TheSeventhbullet/System/MainGameState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "MainGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THESEVENTHBULLET_API AMainGameState : public AGameState
+{
+	GENERATED_BODY()
+};
