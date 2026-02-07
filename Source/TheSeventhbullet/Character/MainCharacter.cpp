c837621c02fbc0ccdc9b0cdb814e6224573262e0
NMEii <82252932+NMEii@users.noreply.github.com>
2026-02-07 20:59:11 +0900

    리베이스용 임시 커밋

---
+++ Source/TheSeventhbullet/Character/MainCharacter.cpp
@@ -0,0 +1,200 @@
+﻿#include "MainCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "MainPlayerController.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AMainCharacter::AMainCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	MaxHP = 100;
+	CurrentHP = MaxHP;
+	MaxStamina = 100;	
+	CurrentStamina = MaxStamina;
+	MaxSpeed = 600.0f;
+	SprintMultifier = 1.5f;
+	
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->bUsePawnControlRotation= true;
+	SpringArm->TargetArmLength = 300.0f;
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+	Camera->bUsePawnControlRotation = false;
+	
+	GetCharacterMovement()->MaxWalkSpeed = MaxSpeed;
+}
+
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (AController* CurrentController = GetController())
+	{
+		// 현재 실제 컨트롤러의 클래스 이름을 출력합니다.
+		UE_LOG(LogTemp, Error, TEXT("Actual Controller Class: %s"), *CurrentController->GetClass()->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("No Controller possessed yet!"));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Enter"));
+	if (AMainPlayerController* PC = Cast<AMainPlayerController>(GetController()))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AMainPlayerController is generated"));
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(PC->InputMappingContext, 0);
+		}
+	}
+	UE_LOG(LogTemp, Warning, TEXT("AMainPlayerController is not generated"));
+}
+
+
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* InputComponents = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (AMainPlayerController* PC = Cast<AMainPlayerController>(GetController()))
+		{
+			// Move 바인딩
+			InputComponents->BindAction(
+				PC->MoveAction,
+				ETriggerEvent::Triggered,
+				this,
+				&AMainCharacter::PlayerMove
+			);
+			
+			// Look 바인딩
+			InputComponents->BindAction(
+				PC->LookAction,
+				ETriggerEvent::Triggered,
+				this,
+				&AMainCharacter::PlayerLook
+			);
+			
+			// StartSprint 바인딩
+			InputComponents->BindAction(
+				PC->SprintAction,
+				ETriggerEvent::Triggered,
+				this,
+				&AMainCharacter::PlayerStartSprint
+			);
+			
+			// StopSprint 바인딩
+			InputComponents->BindAction(
+				PC->SprintAction,
+				ETriggerEvent::Completed,
+				this,
+				&AMainCharacter::PlayerStopSprint
+			);
+			
+			// Dodge 바인딩
+			InputComponents->BindAction(
+				PC->DodgeAction,
+				ETriggerEvent::Triggered,
+				this,
+				&AMainCharacter::PlayerDodge
+			);
+			
+			// Aim 바인딩
+			InputComponents->BindAction(
+				PC->AimAction,
+				ETriggerEvent::Triggered,
+				this,
+				&AMainCharacter::PlayerAim
+			);
+			
+			// Fire 바인딩
+			InputComponents->BindAction(
+				PC->FireAction,
+				ETriggerEvent::Triggered,
+				this,
+				&AMainCharacter::PlayerFire
+			);
+			
+			// Interact 바인딩
+			InputComponents->BindAction(
+				PC->InteractAction,
+				ETriggerEvent::Triggered,
+				this,
+				&AMainCharacter::PlayerInteract
+			);
+			
+			// OpenInventory 바인딩
+			InputComponents->BindAction(
+				PC->OpenInventoryAction,
+				ETriggerEvent::Triggered,
+				this,
+				&AMainCharacter::PlayerOpenInventory
+			);
+		}
+	}
+}
+
+void AMainCharacter::PlayerMove(const FInputActionValue& value)
+{	
+	if (!Controller)	return;
+	
+	const FVector2D MoveInput = value.Get<FVector2D>();
+	
+	if (!FMath::IsNearlyZero(MoveInput.X))
+	{
+		AddMovementInput(GetActorForwardVector(), MoveInput.X);
+	}
+	
+	if (FMath::IsNearlyZero(MoveInput.Y))
+	{
+		AddMovementInput(GetActorRightVector(), MoveInput.Y);
+	}
+}
+
+void AMainCharacter::PlayerLook(const FInputActionValue& value)
+{	
+	const FVector2D LookInput = value.Get<FVector2D>();
+	
+	AddControllerYawInput(LookInput.X);
+	AddControllerPitchInput(LookInput.Y);
+}
+
+void AMainCharacter::PlayerStartSprint(const FInputActionValue& value)
+{
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->MaxWalkSpeed = MaxSpeed * SprintMultifier;
+	}
+}
+
+void AMainCharacter::PlayerStopSprint(const FInputActionValue& value)
+{
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->MaxWalkSpeed = MaxSpeed;
+	}
+}
+
+void AMainCharacter::PlayerDodge(const FInputActionValue& value)
+{
+}
+
+void AMainCharacter::PlayerAim(const FInputActionValue& value)
+{
+}
+
+void AMainCharacter::PlayerFire(const FInputActionValue& value)
+{
+}
+
+void AMainCharacter::PlayerInteract(const FInputActionValue& value)
+{
+}
+
+void AMainCharacter::PlayerOpenInventory(const FInputActionValue& value)
+{
+}
+
---
+++ Source/TheSeventhbullet/Character/MainCharacter.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+struct FInputActionValue;
+
+UCLASS()
+class THESEVENTHBULLET_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMainCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+public:	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character")
+	int32 MaxHP;	// 최대 체력
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Character")
+	int32 CurrentHP;	// 현재 체력
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character")
+	int32 MaxStamina;	// 최대 스테미나
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Character")
+	int32 CurrentStamina;	// 현재 스테미나
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character")
+	float MaxSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character")
+	float SprintMultifier;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character")
+	USpringArmComponent* SpringArm;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character")
+	UCameraComponent* Camera;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	void PlayerMove(const FInputActionValue& value);
+	void PlayerLook(const FInputActionValue& value);
+	void PlayerStartSprint(const FInputActionValue& value);
+	void PlayerStopSprint(const FInputActionValue& value);
+	void PlayerDodge(const FInputActionValue& value);
+	void PlayerAim(const FInputActionValue& value);
+	void PlayerFire(const FInputActionValue& value);
+	void PlayerInteract(const FInputActionValue& value);
+	void PlayerOpenInventory(const FInputActionValue& value);
+};
