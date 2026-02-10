ccc609547f350626e33d2e12c738f47c132a52dc
wjdwngus2056-jpg <wjdwngus2056@gmail.com>
2026-02-10 09:49:34 +0900

    [26/02/09] Revolver, Rifle 발사로직
    Revolver, Rifle 발사로직
    - 라인트레이스 적용
    - Start Fire(), Stop Fire()로 발사의 단계를 나누었음
     - 각 무기 별로 발사키(마우스클릭)를 누르고 있을 때의 발사속도를 제어하기 위함.
     - 다른 무기를 추가할 때의 확장성을 고려.

--- Source/TheSeventhbullet/Weapon/Actor/WeaponBase.h
+++ Source/TheSeventhbullet/Weapon/Actor/WeaponBase.h
@@ -2,17 +2,45 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "TheSeventhbullet/Weapon/Interface/Equipable.h"
+#include "TheSeventhbullet/Weapon/Interface/Fireable.h"
 #include "WeaponBase.generated.h"
 
+class USphereComponent;
+
 UCLASS()
-class THESEVENTHBULLET_API AWeaponBase : public AActor
+class THESEVENTHBULLET_API AWeaponBase
+	: public AActor,
+	  public IFireable,
+	  public IEquipable
 {
 	GENERATED_BODY()
 
 public:
 	AWeaponBase();
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Component")
+	TObjectPtr<USceneComponent> Scene;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	TObjectPtr<USphereComponent> Collision;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Component")
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+	
+	virtual void Equip(TObjectPtr<AActor> NewOwner) override;
+	virtual void UnEquip() override;
+	
+	virtual void StartFire() override;
+	virtual void StopFire() override;
+	virtual void Reload() override;
+	
+	virtual void Fire();
+	void ConsumeAmmo();
 
 protected:
-	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
+	int32 MaxAmmo = 6;
+	int32 CurrentAmmo = 0;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
+	TObjectPtr<AActor> WeaponOwner;
+	
 };
