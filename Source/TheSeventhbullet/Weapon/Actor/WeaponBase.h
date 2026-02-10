89f727b7927b74cefd4b88ebe73d86ef5e263505
wjdwngus2056-jpg <wjdwngus2056@gmail.com>
2026-02-10 21:48:08 +0900

    [26/02/10] 주석추가 / 디버그 관련 변수 추가
    주석추가
    bDrawDebugInfinite : 디버그 드로우를 계속해서 표시할 것인지 여부를 변수로 만들어 사용

--- Source/TheSeventhbullet/Weapon/Actor/WeaponBase.h
+++ Source/TheSeventhbullet/Weapon/Actor/WeaponBase.h
@@ -40,6 +40,8 @@ protected:
 	bool bDrawFireDebug = true; // 발사 디버그 표시 여부
 	UPROPERTY(EditAnywhere, Category = "Weapon")
 	float FireDebugDuration = 1.0f; // 발사 디버그 지속 시간
+	UPROPERTY(EditAnywhere, Category = "Weapon")
+	bool bDrawDebugInfinite = false; // 발사 디버그 드로우를 영구지속할지 여부
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
 	TObjectPtr<AActor> WeaponOwner;
@@ -51,12 +53,12 @@ protected:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
 	TObjectPtr<UWeaponDataAsset> WeaponDataAsset;
-	float Damage = 10.f;
-	float FireInterval = 0.5f;
-	float Range = 850.f;
-	int32 MaxAmmo = 6;
-	int32 CurrentAmmo = 0;
-	float ReloadTime = 1.0f;
-	int32 AmountOfPellets = 1;
-	float PelletSpreadRadius = 3.f;
+	float Damage = 10.f; // 데미지 베이스
+	float FireInterval = 0.5f; // 발사 간격(발사속도)
+	float Range = 850.f; // 사거리
+	int32 MaxAmmo = 6; // 탄창 최대치
+	int32 CurrentAmmo = 0; // 현재 탄창
+	float ReloadTime = 1.0f; // 재장전 시간
+	int32 AmountOfPellets = 1; // 발사체 갯수
+	float PelletSpreadRadius = 3.f; // 탄 퍼짐 정도
 };
