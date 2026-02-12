7b71f5e6d9ae2218f3f8d794f23452af4a3a8d20
NMEii <82252932+NMEii@users.noreply.github.com>
2026-02-12 20:28:39 +0900

    Skill 데미지 / 넉백 구현
    - Enemy Tag 있는 적만 데미지 및 넉백 적용
    - 사운드 / 파티클 적용

--- Source/TheSeventhbullet/Character/PlayerSkill.h
+++ Source/TheSeventhbullet/Character/PlayerSkill.h
@@ -19,10 +19,40 @@ protected:
 	virtual void BeginPlay() override;
 
 public:
+#pragma region Components
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Component")
 	USphereComponent* Collision;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Component")
 	UStaticMeshComponent* Mesh;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Component")
 	UProjectileMovementComponent* ProjectileMovement;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effect")
+	USoundBase* ExplodeSound;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effect")
+	UParticleSystem* ExplodeParticle;
+	
+#pragma endregion
+	
+#pragma region Explosion
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Explode")
+	float ExplodeRadius;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Explode")
+	float DamageAmount;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Explode")
+	float KnockBackAmount;
+	
+	UFUNCTION()
+	void OnHit(
+		UPrimitiveComponent* HitComp, 
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit
+		);
+	
+	void Explode();
+	
+#pragma endregion
 };
