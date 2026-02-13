a6ac291913eda16e9b3034b7ca22cdd0003fc09b
khs081215 <39721769+khs081215@users.noreply.github.com>
2026-02-13 19:43:53 +0900

    add BehaviorTree Service, Task Nodes
    피격 애니메이션을 기획에 맞게 작동하게 만들기 위해 다음 BT 서비스, 태스크를 추가했습니다.
    1. HitEnd Service : BT에서 해당 노드를 탈출하려고 할 때 실행되어 피격중인 상태를 해제합니다.
    2. Hit Task : Hit 애님몽타주를 실행하며, 그동안 다시 애님몽타주가 실행되지 않게 합니다.

---
+++ Source/TheSeventhbullet/Enemy/BehaviorTree/Service/BTService_HitEnd.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_HitEnd.generated.h"
+
+/**
+ * 단순히 Hit되었을시 진입하여 Hit을 false로 바꾸어 준다.
+ */
+UCLASS()
+class THESEVENTHBULLET_API UBTService_HitEnd : public UBTService
+{
+	GENERATED_BODY()
+	UBTService_HitEnd();
+	
+protected:
+	virtual void OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
