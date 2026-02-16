6063a3c867c5e773f559cba1c0624ca3af7e5847
humilshin <quantum0724@gmail.com>
2026-02-16 17:58:36 +0900

    Update AsyncDataManager Delegate
    Update Delegate

--- Source/TheSeventhbullet/Manager/AsyncDataManager.h
+++ Source/TheSeventhbullet/Manager/AsyncDataManager.h
@@ -7,25 +7,27 @@
 #include"Engine/StreamableManager.h"
 #include"AsyncDataManager.generated.h"
 
+DECLARE_DELEGATE(FOnBundleLoadComplete);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* [AsyncDataManager Guide]
  * 1. Load : LoadBundles로 타입의 DataAsset + 번들 리소스를 비동기 로드
  * 2. Load : UnloadBundles / UnloadAllByType으로 해제
  *
  * 번들 예시 :
- * 	LoadSingleBundle(FPrimaryAssetType("Item"), NAME_None, true);
- * 	LoadSingleBundle(FPrimaryAssetType("Item"), FName("Lobby"), true);
- * 	
- *  *** 사용 예시 : 
+ * 	LoadBundles(FPrimaryAssetType("Item"), {}, OnComplete);          // 메타데이터만
+ * 	LoadBundles(FPrimaryAssetType("Item"), {"Lobby"}, OnComplete);   // Lobby 번들
+ *
+ *  *** 사용 예시 :
  *  UAsyncDataManager* Mgr = UAsyncDataManager::Get(this);
- *  if (Mgr->IsInitialLoadComplete()) // Initial Load로 Lobby 관련 데이터 및 메타데이터 로드
+ *  if (Mgr->IsAssetLoaded(ItemID))
  *  {
  *      UItemDataAsset* Data = Cast<UItemDataAsset>(Mgr->GetLoadedAsset(ItemID));
- *      UTexture2D* Icon = Data->Icon.Get();  // Texture나 Image 같은 경우 Get을 통해 반환 가능
+ *      UTexture2D* Icon = Data->Icon.Get();
  *  }
  *
- * Load 및 Unload 예시 
- *  던전 입장: Mgr->LoadBundles(FPrimaryAssetType("Item"), {"InGame"});
+ * Load 및 Unload 예시
+ *  던전 입장: Mgr->LoadBundles(FPrimaryAssetType("Item"), {"InGame"}, OnComplete);
  *  던전 퇴장: Mgr->UnloadBundles(FPrimaryAssetType("Item"), {"InGame"});
 */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -40,19 +42,12 @@ public:
 	virtual void Deinitialize() override;
 
 	static UAsyncDataManager* Get(const UObject* WorldContextObject);
-	
-	UFUNCTION(BlueprintCallable, Category = "Data|Async")
-	void LoadBundles(FPrimaryAssetType AssetType, const TArray<FName>& Bundles);
-	
-	UFUNCTION(BlueprintCallable, Category = "Data|Async")
+
+	void LoadBundles(FPrimaryAssetType AssetType, const TArray<FName>& Bundles, FOnBundleLoadComplete OnComplete);
 	void UnloadBundles(FPrimaryAssetType AssetType, const TArray<FName>& Bundles);
-
-	UFUNCTION(BlueprintCallable, Category = "Data|Async")
 	void UnloadAllByType(FPrimaryAssetType AssetType);
-
-	UFUNCTION(BlueprintPure, Category = "Data|Async")
-	bool IsInitialLoadComplete() const;
-
+	
+public:
 	UFUNCTION(BlueprintCallable, Category = "Data|Async")
 	UPrimaryDataAsset* GetLoadedAsset(FPrimaryAssetId AssetID) const;
 
@@ -68,10 +63,5 @@ private:
 
 	TMap<FPrimaryAssetType, TMap<FName, TSharedPtr<FStreamableHandle>>> BundleHandles;
 
-	bool bInitialLoadComplete = false;
-	int32 PendingInitialLoadCount = 0;
-
-	void CheckInitialLoadComplete();
-	
-	void LoadSingleBundle(FPrimaryAssetType AssetType, FName BundleName, bool bTrackCompletion = false);
+	void LoadSingleBundle(FPrimaryAssetType AssetType, FName BundleName, FOnBundleLoadComplete OnComplete);
 };
