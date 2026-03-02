565ed7c634f09a101175285459a02abb92183940
HumilityShin <quantum0724@gmail.com>
2026-03-03 02:08:39 +0900

    Chnage CrossHair

--- Source/TheSeventhbullet/UI/CrosshairWidget.h
+++ Source/TheSeventhbullet/UI/CrosshairWidget.h
@@ -35,10 +35,10 @@ public:
 	float HitMarkerDuration = 0.15f;
 
 	UPROPERTY(EditDefaultsOnly, Category = "UI|Crosshair")
-	FLinearColor NormalHitColor = FLinearColor::White;
+	FLinearColor NormalHitColor = FLinearColor(10.0f, 10.0f, 10.0f, 1.0f);
 
 	UPROPERTY(EditDefaultsOnly, Category = "UI|Crosshair")
-	FLinearColor HeadShotColor = FLinearColor::Red;
+	FLinearColor HeadShotColor = FLinearColor(10.0f, 0.0f, 0.0f, 1.0f);
 
 protected:
 	virtual void NativeConstruct() override;
