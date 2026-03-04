9c5a78acecdc7e663663604456eebdc9e62d1c2a
HumilityShin <quantum0724@gmail.com>
2026-03-04 17:23:20 +0900

    Reset Stat

--- Source/TheSeventhbullet/Character/Component/StatusComponent.cpp
+++ Source/TheSeventhbullet/Character/Component/StatusComponent.cpp
@@ -9,7 +9,7 @@ UStatusComponent::UStatusComponent()
 	CharacterStatus.Speed = 600.0f;
 	CharacterStatus.HP = 100;
 	CharacterStatus.Stamina = 100;
-	CharacterStatus.Attack = 150;
+	CharacterStatus.Attack = 10;
 	CharacterStatus.Defence = 0;
 	CharacterStatus.CriticalChance = 0.15f;
 	CharacterStatus.CriticalDamage = 1.5f;
